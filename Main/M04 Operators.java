707089091cf30a06d39f34087785b91bffa4b996
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-08-07 21:48:59 -0300

    Create M04 Operators.java

---
+++ Main/M04 Operators.java
@@ -0,0 +1,13 @@
+//Operators
+
+// % modulus(mod) / ++ increment / -- decrement
+
+public class Main{
+    public static void main(String[] args){
+        int x = 100 + 50;       //150
+        int sum1 = x + 250;     //400
+        int sum2 = sum1 + sum1; //800
+        int a = 10; //assignment
+        a += 5;     //addition assignment
+    }
+}
