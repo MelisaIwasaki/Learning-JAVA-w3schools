fc0bfe834fc497a2d75b3869f9796fdfd5020ecc
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-08-08 17:17:30 -0300

    Create M09 Boolean.java

---
+++ Main/M09 Boolean.java
@@ -0,0 +1,14 @@
+//boolean expression
+
+public class Main{
+    public static void main(String[] args){
+        int x = 10;
+        int y = 9;
+        
+        System.out.println(x > y);   //return true, because 10 is higher than 9.
+        System.out.println(x == 10); //return true, because the value of x is equal to 10.
+        System.out.println(10 == 15);//return false, because 10 is not equal to 15.
+        
+        
+    }
+}
