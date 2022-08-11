660cc8edddb0e036a77f4ba01aec7ca261579500
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-08-11 11:04:38 -0300

    Update C001 OOP.java

--- Classes/C001 OOP.java
+++ Classes/C001 OOP.java
@@ -14,6 +14,22 @@ So, a class is template for objects, and an object is an instance of a class.
 
 A class is like an object constructor or a "blueprint" for creating objects.*/
 
+
+/*OOP Programación Orientada a Objetos
+
+Se trata de crear objetos que contienen tanto datos como métodos.
+OOP es mas rápido y mas fácil de ejecutar.
+OOP proporciona una estructura clara para los programas.
+OOP ayuda a mantener el código Java "No se repita",y hace que el código sea más fácil de mantener,
+modificar y depurar.
+OOP hace posible crear aplicaciones reutilizables completas con menos código y un tiempo de desarrollo más corto.
+
+Entonces, una clase es una plantilla para objetos y un objeto es una instancia de clases.
+
+Cuando se crean los objetos individuales,heredan todas las variables y métodos de la clase.*/
+
+
+
 //Create an objects called "myObj" and print the value of x. 
 
 public class Main{
