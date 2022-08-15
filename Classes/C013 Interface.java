1acfeffca275ca53e1d666a3e25d799f15cfb60c
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-08-15 17:04:11 -0300

    Update C013 Interface.java

--- Classes/C013 Interface.java
+++ Classes/C013 Interface.java
@@ -61,3 +61,28 @@ Por qué y cuándo usar intefaces?
 con interfaces,porque la clase puede implementar múltiples interfaces.*/
 
 /*******************************************************************************/
+
+//Multiple interfaces
+
+interface FirstInterface{
+    public void myMethod(); //interface method
+}
+interface SecondInterface{
+    public void myOtherMethod();
+}
+class DemoClass implements FirstInterface, SecondInterface{
+    public void myMethod(){
+        System.out.println("Some text..");
+    }
+    public void myOtherMethod(){
+        System.out.println("Some other text..");
+    }
+}
+class Main{
+    public static void main(String[] args){
+        DemoClass myObj = new DemoClass();
+        myObj.myMethod();
+        myObj.myOtherMethod();
+    }
+}
+
